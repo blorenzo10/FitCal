9702f5abee42eb426bd9fb4832f5a50f98c65bb3
blorenzo10 <bruno.lorenzop@gmail.com>
2017-06-21 17:54:38 -0300

    First commit

---
+++ FitCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitCal
+//
+//  Created by Natalia Consonni on 21/6/17.
+//  Copyright © 2017 Bruno Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
